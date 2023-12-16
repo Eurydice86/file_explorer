d5a3e941c758422e125580fa8958fb08987fbb14
Eurydice86 <kyriakopoulos.nikos@gmail.com>
2023-12-16 12:34:53 +0200

    flags list (not yet doing anything) and printing sorted contents in a valid path

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
 #include <string>
 #include <iostream>
 #include <filesystem>
+#include <vector>
+#include <functional>
+#include <algorithm>
 
 namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]) {
+
+  std::vector<std::string> flags;
 
   fs::path p = "./";
   if (argc > 1) {
     p += argv[1];
   }
 
-  std::cout << p << "\n";
+  for (int i = 1; i < argc; i++) {
+    std::string s(argv[i]);
+    if (s[0] == '-') {
+      flags.push_back(s.substr(1));
+    }
+  }
+
+  std::string path(p);
+  int path_buffer = 1;
+  if (path[path.size() - 1] == '/') {
+    path_buffer = 0;
+  }
+  
+  std::vector<std::string> output_list;
   for (const auto &entry : fs::directory_iterator(p)) {
-    std::cout << entry.path() << "\n";
+    std::string file_or_directory = entry.path();
+    file_or_directory = file_or_directory.substr(path.size() + path_buffer); // removing the path from the beginning
+    output_list.push_back(file_or_directory);
+  }
+  std::sort(output_list.begin(), output_list.end());
+
+  for (auto o : output_list) {
+    std::cout << o  << "\n";
   }
   
 }
