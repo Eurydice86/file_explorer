4712745f67c19880e69534d1cec6b3041645aa5f
Eurydice86 <kyriakopoulos.nikos@gmail.com>
2023-12-18 14:26:47 +0200

    added std::expected stuff

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <functional>
+#include <expected>
+#include <tuple>
+#include <string>
+
+struct path_flags {
+  std::string path;
+  std::vector<std::string> flags;
+};
+
+enum class error {
+  compile_time_error,
+  parse_error
+};
+
+//path_flags parser(std::vector<std::string> args);
+auto parser(std::vector<std::string> args) -> std::expected<path_flags, error>;
 
 
-std::tuple<std::string, std::vector<std::string>> parser(std::vector<std::string>);
 std::vector<std::string> output(std::string, std::vector<std::string>);
 
 int main(int argc, char* argv[]) {
@@ -10,12 +25,22 @@ int main(int argc, char* argv[]) {
   for (int i = 1; i < argc; i++) {
     args.push_back(argv[i]);
   }
+
+  std::string path;
+  std::vector<std::string> flags;
   
-  auto [path, flags] = parser(args);
-  
+  auto pf = parser(args);
+  if (!pf.has_value())
+    std::cout << "Error"  << "\n";
+  else {
+    path = pf.value().path;
+    flags = pf.value().flags;
+  }
   auto output_list = output(path, flags);
-  
+
   for (auto o : output_list) {
     std::cout << o << "\n";
   }
-} 
+  for (auto f : flags)
+    std::cout << f << "\n";
+}
--- parser.cpp
+++ parser.cpp
@@ -1,23 +1,45 @@
 #include <string>
+#include <expected>
 #include <vector>
 #include <tuple>
+#include <iostream>
+
+struct path_flags {
+  std::string path;
+  std::vector<std::string> flags;
+};
+
+enum class error {
+  compile_time_error,
+  parse_error
+};
+
 // The parser function reads the arguments given and returns:
 // - a string, which is the path for which the contents will be shown
 // - a vector of strings, which are the flags (i.e. options), which start with '-'
-
-std::tuple<std::string, std::vector<std::string>> parser(std::vector<std::string> args) {
+auto parser(std::vector<std::string> args) -> std::expected<path_flags, error> {
+  //path_flags parser(std::vector<std::string> args) {
   std::string path = "./";
   std::vector<std::string> flags;
+
+  int path_counter = 0;
   
   for (auto arg : args) {
     if (arg[0] == '-') {
       flags.push_back(arg.substr(1));
     } else {
       path += arg;
+      path_counter++;
+      if (path_counter > 1)
+	return std::unexpected(error::parse_error);
     }
   }
 
-  return std::make_tuple(path, flags);
+  path_flags pf;
+  pf.path = path;
+  pf.flags = flags;
+
+  return pf;
 }
 
 // path
